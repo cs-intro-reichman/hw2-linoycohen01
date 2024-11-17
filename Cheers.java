7fc4ae2429c5e1968302038c0d375629a2a8fc43
linoycohen01 <linoy.cohen01@post.runi.ac.il>
2024-11-17 19:28:58 +0200

    second try

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,20 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String Cheer = args[0];
+            String CapsLetter = Cheer.toUpperCase();
+            int times = Integer.parseInt(args[1]);
+            String letters = "AEFHILMNORSX";
+            System.out.println(CapsLetter + times);
+            for (int i = 0; i < CapsLetter.length(); i++)
+            {
+                char current = CapsLetter.charAt(i);
+                System.out.println("Give me " + ((letters.indexOf(CapsLetter.charAt(i)) == -1) ? "a  " : "an ")
+                                         + current + ": " + current + "!");
+            }
+            System.out.println("What does that spell?");
+            for (int i = 0; i < times; i++){
+                System.out.println(CapsLetter + "!!!");
+            }
         }
 }
